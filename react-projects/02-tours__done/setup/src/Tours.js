8214172bbb4acb526b37ecbfbebedd50e999682a
moohameed <mohamed.trabelsi@esen.tn>
2021-12-23 09:22:16 +0100

    final

---
+++ react-projects/02-tours__done/setup/src/Tours.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours , removeTour}) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2> our tours</h2>
+        <div className='underline'></div>
+      </div>
+    <div>
+      {tours.map ((item) => {
+        return <Tour key={item.id} {...item}  removeTour = {removeTour}></Tour>
+      })}
+    </div>
+    </section>
+  );
+};
+
+export default Tours;
